ccdf819c7cbb751ad44d3f0e48aa1160294761c6
Samuel Rebeles <samuel.rebeles@wholefoods.com>
2018-01-03 23:06:38 -0600

    Adding the ability to add html from a get request

--- flask_app.py
+++ flask_app.py
@@ -8,24 +8,24 @@ import json
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-
 recipe_data = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="c2c_login",
     password="38Ua2ZrCuXws",
-    hostname="c2c-dev-db.coos5kthp77n.us-west-2.rds.amazonaws.com",
+    hostname="c2c-db.coos5kthp77n.us-west-2.rds.amazonaws.com",
     databasename="flask_app")
-
-engine = create_engine(recipe_data, convert_unicode=True)
-comments = []
-results = []
 
 @app.route('/', methods=["GET"])
 def index():
     return render_template("main_page.html")
         
-@app.route('/recipes/', methods=["GET"])
+@app.route('/recipes', methods=["GET"])
 def get_recipes():
-    zip_code = request.args.get["zip_code"]
-    cuisine_pref = request.args.get["cuisine_pref"]
-    results = engine.execute('select * from RecipeData where zip_code = %s OR cuisine_pref = %s;'.format(zip_code, cuisine_pref))
-    return json.dumps(results)
+    title = request.args.get("name")
+    category = request.args.get("category")
+    url= request.args.get("url")
+
+    engine = create_engine(recipe_data)
+    sql_query_string = "select * from RecipeData where name LIKE '%{0}%' OR url = '{1}' OR category = '{2}';".format(title, url, category)
+    print(sql_query_string)
+    results = engine.execute(sql_query_string)
+    return json.dumps([(dict(row.items())) for row in results])
